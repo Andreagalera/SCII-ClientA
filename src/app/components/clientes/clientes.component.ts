017c9c648d3b572b49e61dbeda07c39f4a4d87c9
Andreagalera <1d09galeraandrea@gmail.com>
2020-03-28 12:09:56 +0100

    Comunication between A-B and A-TTP

--- src/app/components/clientes/clientes.component.ts
+++ src/app/components/clientes/clientes.component.ts
@@ -5,8 +5,13 @@ import { NgForm } from "@angular/forms";
 import * as bcu from "bigint-crypto-utils";
 import * as bac from "bigint-conversion";
 import * as test from "rsa";
-import {HexBase64BinaryEncoding} from "crypto";
+import { HexBase64BinaryEncoding } from "crypto";
+import { timeout } from 'q';
 const sha = require('object-sha');
+import * as CryptoJS from 'crypto-js';
+
+
+
 
 declare var M: any;
 
@@ -21,6 +26,8 @@ export class ClientesComponent implements OnInit {
   dataReceive: string;
 
   publicKey: test.PublicKey;
+  publicKeyTTP: test.PublicKey;
+
   keyPair: test.KeyPair;
 
   decrypted: string;
@@ -29,16 +36,23 @@ export class ClientesComponent implements OnInit {
   _ONE: BigInt = BigInt(1);
   response: string;
 
-  constructor(private clientService: ClienteService) {}
+  encriptKey: CryptoKey;
+
+  constructor(private clientService: ClienteService) { }
 
   async ngOnInit() {
-    // this.getPublicKey();
 
+    // Genera key pair de A
     this.keyPair = await test.generateRandomKeys();
     console.log(this.keyPair);
+
+    // Llamar función obtiene public key de B y TTP
     await this.getPublicKey();
+    await this.getPublicKeyTTP();
+
   }
 
+  // Función para obtener la public key de B
   async getPublicKey() {
     this.clientService.getData().subscribe(data => {
       this.publicKey = new test.PublicKey(
@@ -46,6 +60,16 @@ export class ClientesComponent implements OnInit {
         bac.hexToBigint(data.n)
       );
       this.dataReceive = this.publicKey.n;
+    });
+  }
+
+  // Función para obtener la public key de TTP
+  async getPublicKeyTTP() {
+    this.clientService.getPublicKeyTTP().subscribe(data => {
+      this.publicKeyTTP = new test.PublicKey(
+        bac.hexToBigint(data.e),
+        bac.hexToBigint(data.n)
+      );
     });
   }
 
@@ -83,23 +107,140 @@ export class ClientesComponent implements OnInit {
     const b = await bac.bigintToHex(
       (m * this.publicKey.encrypt(this.r)) % this.publicKey.n
     );
-    // const digest = await sha.digest(b, 'SHA-256');
-    const message = {
-      msg: b
+    // Criptograma
+    // const c = this.encriptKeyFnc(b);
+    var ts = new Date();
+    const body = {
+      type: "1",
+      src: "A",
+      dest: "B",
+      msg: b,
+      timestamp: ts
     };
-    // const digest = await sha.digest(message, 'SHA-256');
+    // Signature
+    const digest = await sha.digest(body, 'SHA-256');
+    const digestHex = await bac.hexToBigint(digest);
+    let signature = this.keyPair.privateKey.sign(digestHex);
+    signature = await bac.bigintToHex(signature);
+    const message = {
+      body: body,
+      signature: signature
+    };
+
     this.clientService.post_message_sign(message).subscribe(async res => {
-      const bs = bac.hexToBigint(res['msg']);
+      console.log(res);
+      const bs = bac.hexToBigint(res['body']['msg']);
       const s =
         (await (bs * bcu.modInv(this.r, this.publicKey.n))) % this.publicKey.n;
       const m = await this.publicKey.verify(s);
-      let m1= bac.bigintToText(m);
-      console.log(m1);
+      let m1 = bac.bigintToText(m);
+      // console.log(m1);
       document.getElementById(
         "blind-sign-verified"
       ).innerHTML = ("The message verified is: " +
         bac.bigintToText(m));
+
+      // Si el mensaje de B se recibe como que quiere la k
+      if (res['body']['type'] == 2) {
+        // Llamar a servicio que envie a TTP k
+        var tsTTP = new Date();
+        const k = 2;
+        const body = {
+          type: "3",
+          src: "A",
+          dest: "B",
+          ttp: "TTP",
+          k: k,
+          timestamp: tsTTP
+        };
+        // Signature
+        const digestTTP = await sha.digest(body, 'SHA-256');
+        const digestHexTTP = await bac.hexToBigint(digestTTP);
+        let signatureTTP = this.keyPair.privateKey.sign(digestHexTTP);
+        signatureTTP = await bac.bigintToHex(signatureTTP);
+        const messageTTP = {
+          body: body,
+          signature: signatureTTP
+        };
+
+        this.clientService.sendK(messageTTP).subscribe(async resTTP =>{
+          console.log("B ya puede saber C");
+          console.log(resTTP);
+        });
+
+
+      }
     });
+  }
+
+
+  // Criptograma
+  async encriptKeyFnc(data) {
+    window.crypto.subtle.importKey(
+      "jwk", //can be "jwk" or "raw"
+      {   //this is an example jwk key, "raw" would be an ArrayBuffer
+        kty: "oct",
+        k: "Y0zt37HgOx-BY7SQjYVmrqhPkO44Ii2Jcb9yydUDPfE",
+        alg: "A256CBC",
+        ext: true,
+      },
+      {   //this is the algorithm options
+        name: "AES-CBC",
+        length: 256,
+      },
+      false, //whether the key is extractable (i.e. can be used in exportKey)
+      ["encrypt", "decrypt"] //can be "encrypt", "decrypt", "wrapKey", or "unwrapKey"
+    )
+      .then(function (key) {
+        //returns the symmetric key
+        // this.encriptKey = key;
+        console.log(key);
+        window.crypto.subtle.encrypt(
+          {
+            name: "AES-CBC",
+            //Don't re-use initialization vectors!
+            //Always generate a new iv every time your encrypt!
+            iv: window.crypto.getRandomValues(new Uint8Array(16)),
+          },
+          key, //from generateKey or importKey above
+          data //ArrayBuffer of data you want to encrypt
+        )
+          .then(function (encrypted) {
+            //returns an ArrayBuffer containing the encrypted data
+            console.log(new Uint8Array(encrypted));
+            return encrypted;
+          })
+        // .catch(function (err) {
+        //   console.error(err);
+        // });
+      });
+    // return encriptKey;
+
+  }
+  // .catch(function (err) {
+  //   console.error(err);
+  // });
+
+
+  async encriptK(data, encriptKey) {
+    window.crypto.subtle.encrypt(
+      {
+        name: "AES-CBC",
+        //Don't re-use initialization vectors!
+        //Always generate a new iv every time your encrypt!
+        iv: window.crypto.getRandomValues(new Uint8Array(16)),
+      },
+      encriptKey, //from generateKey or importKey above
+      data //ArrayBuffer of data you want to encrypt
+    )
+      .then(function (encrypted) {
+        //returns an ArrayBuffer containing the encrypted data
+        console.log(new Uint8Array(encrypted));
+        return encrypted;
+      })
+    // .catch(function (err) {
+    //   console.error(err);
+    // });
   }
 
 
--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -9,20 +9,30 @@ import * as test from 'rsa';
 })
 export class ClienteService {
   nombre: string;
-  readonly URL_API = 'http://localhost:3000/api/clientes';
+  readonly URL_APIB = 'http://localhost:3000/api/clientes';
+  readonly URL_APITTP = 'http://localhost:3001/api/clientes';
+
 
   constructor(private http: HttpClient) { }
 
   getData(): Observable<test.PublicKey> {
-    return this.http.get<test.PublicKey>(this.URL_API);
+    return this.http.get<test.PublicKey>(this.URL_APIB);
+  }
+
+  getPublicKeyTTP(): Observable<test.PublicKey> {
+    return this.http.get<test.PublicKey>(this.URL_APITTP + '/publicKeyTTP');
   }
 
   postData(body: object) {
-    return this.http.post(this.URL_API, body);
+    return this.http.post(this.URL_APIB, body);
   }
 
   post_message_sign(body: object) {
-    return this.http.post(this.URL_API + '/sign', body);
+    return this.http.post(this.URL_APIB + '/sign', body);
+  }
+
+  sendK(body: object) {
+    return this.http.post(this.URL_APITTP + '/msg3', body);
   }
 
 }
